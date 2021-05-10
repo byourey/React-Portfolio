94e514d57337c97f37ccf68ad5097c37e86fe22a
Bjorn Yourey <byourey7783@gmail.com>
2021-05-10 10:41:01 -0400

    updated files

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Technologies from "./components/Technologies";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        BJORN YOUREY
+      </header>
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Technologies.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Card, CardImg, CardDeck, CardBody
+} from 'reactstrap';
+
+function Technologies() {
+  return (
+    <CardDeck>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          
+        </CardBody>
+      </Card>
+    </CardDeck>
+  );
+};
+
+export default Technologies;
